ffcf201c289f4ce2a46699c2260ba592cdfd61ee
norkitorki <norki@tuta.io>
2022-05-17 11:48:11 +0200

    Add create action to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comments_params)
+    redirect_to @article
+  end
 end
