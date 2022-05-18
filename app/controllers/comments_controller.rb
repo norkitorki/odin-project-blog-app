fb490474b39e59fbe8bf92b91d9dae5520babfd7
norkitorki <norki@tuta.io>
2022-05-18 08:58:28 +0200

    Add before_action find_article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :find_article
+
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to @article
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to @article, status: 303
   end
 
   private
+
+  def find_article
+    @article = Article.find(params[:article_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:commenter, :body, :status)
