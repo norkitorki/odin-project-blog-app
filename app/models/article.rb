3afec7c7955201781443ec7ab5d9bb8f692bb5db
norkitorki <norki@tuta.io>
2022-05-20 13:50:12 +0200

    Add a has_many association between article and tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   include Visible
 
   has_many :comments, dependent: :destroy
+  has_many :tags
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
