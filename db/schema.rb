60cbd552565f55bbe36a693ecfdc479f03a155ed
norkitorki <norki@tuta.io>
2022-05-20 13:45:19 +0200

    Migrate tag model into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_17_114050) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_20_114220) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -29,5 +29,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_17_114050) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_tags_on_article_id"
+  end
+
   add_foreign_key "comments", "articles"
+  add_foreign_key "tags", "articles"
 end
