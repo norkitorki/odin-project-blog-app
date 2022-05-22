fc16935d52318851f58abe6b80e646284e6086cb
norkitorki <norki@tuta.io>
2022-05-22 08:00:25 +0200

    Create show,new and create actions

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,26 @@
 class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+    @articles = Article.all.select { |article| article.id == @tag.article_id }
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+
+    if @tag.save
+      redirect_to tag_path(@tag)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
