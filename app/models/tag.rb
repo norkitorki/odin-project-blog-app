8535da32d9c27f37fbf6417163b369a10ca1bf78
norkitorki <norki@tuta.io>
2022-05-22 08:16:56 +0200

    Add tag name validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ApplicationRecord
   belongs_to :article
+
+  validates :name, presence: true, uniqueness: true, format: { with: /\A[a-z]+\z/, 
+                                                               message: "tag must consist of a single word and be lowercase" 
+                                                             }
 end
