660312d96e58d1e5908cbc1017f55063f666e209
norkitorki <norki@tuta.io>
2022-05-25 13:17:45 +0200

    Update schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_20_114220) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_091436) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.string "article_tags"
+  end
+
+  create_table "articles_tags", id: false, force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "tag_id", null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -31,12 +37,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_20_114220) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
-    t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_tags_on_article_id"
   end
 
   add_foreign_key "comments", "articles"
-  add_foreign_key "tags", "articles"
 end
