413f70463ebc1c16dd6f46f7d5f265ed1c019efa
norkitorki <norki@tuta.io>
2022-05-30 13:43:24 +0200

    Update schema with removed comments status column

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_091436) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_104345) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.string "article_tags"
+    t.string "status"
   end
 
   create_table "articles_tags", id: false, force: :cascade do |t|
@@ -31,7 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_091436) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
