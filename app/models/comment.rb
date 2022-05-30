2b60105ff987d5101443fbf60febece7bd5fe00d
norkitorki <norki@tuta.io>
2022-05-30 11:06:20 +0200

    Validate commenter and body

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
-  include Visible
-
   belongs_to :article
+
+  validates :commenter, presence: true, length: { maximum: 30 }
+  validates :body, presence: true
 end
